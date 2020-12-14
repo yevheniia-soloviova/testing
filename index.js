ed19e6613973dcfc0c888a132f8e9d531fa6f6da
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-12-14 22:09:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const makePromise = () => {
     return new Promise((resolve, reject) => {
-    const passed = Math.random() > 0.5;
-    if (passed) {
+        const passed = Math.random() > 0.5;
+        
+        setTimeout(() => {
+            if (passed) {
         resolve('Hi this is resolve');
     }
 
     reject('everything is bad');
+
+        }, 2000);
+   
 });
 };
 
